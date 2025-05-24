ac41c850c29f36ea4769af7fb6f720afc7e1e501
Saimur Rahman <saimur.cdk@gmail.com>
2025-05-25 03:53:38 +0600

    Made Some Basic Designing by colorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,33 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor:const Color.fromARGB(255, 96, 59, 181));
+
 void main(){
   runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+    colorScheme: kColorScheme,
+      appBarTheme: AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer
+        )
+      ),
+      cardTheme: CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical:  8),
+      ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        )
+    ),
     debugShowCheckedModeBanner: false,
     home: Expenses(),
       ));
